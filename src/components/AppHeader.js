380268cbf71427a8218b549f9f2a186f2b11d237
CrystalNyein <nyeinchanlay99@gmail.com>
2022-05-30 14:31:12 +0630

    Nav bar html structure

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,7 +1,31 @@
 import React from "react";
+import logo from "../assets/images/Logo.png";
+import "./AppHeader.css";
 
 const AppHeader = () => {
-  return <div className="AppHeader"></div>;
+  return (
+    <div className="AppHeader">
+      <div className="nav-bar">
+        <div className="nav-logo">
+          <img src={logo} alt="CN"></img>
+        </div>
+        <ul className="nav-link">
+          <li className="nav-item">
+            <a href="">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="">About me</a>
+          </li>
+          <li className="nav-item">
+            <a href="">My Projects</a>
+          </li>
+          <li className="nav-item">
+            <a href="">Hire me</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default AppHeader;
