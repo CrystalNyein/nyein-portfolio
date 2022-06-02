cd88fd168d890c1ab0a221f95dddc47eb01245f0
CrystalNyein <nyeinchanlay99@gmail.com>
2022-06-02 16:09:33 +0630

    Mobile toggle menu done

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,28 +1,32 @@
-import React, { useState } from "react";
-import logo from "../assets/images/Logo.png";
+import React, { useState, useRef } from "react";
 import "./AppHeader.css";
 
 const AppHeader = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const navLink = useRef(null);
   const toggleMenu = (e) => {
     if (menuOpen) {
       e.target.classList.remove("fa-xmark");
       e.target.classList.add("fa-burger");
+      navLink.current.classList.remove("active");
     } else {
       e.target.classList.remove("fa-burger");
       e.target.classList.add("fa-xmark");
+      navLink.current.classList.add("active");
     }
     setMenuOpen(!menuOpen);
   };
   return (
     <div className="AppHeader">
       <div className="nav-bar">
-        <div className="nav-logo">CN</div>
-        <div className="nav-action" onClick={toggleMenu}>
-          <i className="fa-solid fa-burger"></i>
+        <div className="nav-mobile">
+          <div className="nav-logo">CN</div>
+          <div className="nav-action" onClick={toggleMenu}>
+            <i className="fa-solid fa-burger"></i>
+          </div>
         </div>
-        <ul className="nav-link">
-          <li className="nav-item">
+        <ul className="nav-link" ref={navLink}>
+          <li className="nav-item current">
             <a href="">Home</a>
           </li>
           <li className="nav-item">
