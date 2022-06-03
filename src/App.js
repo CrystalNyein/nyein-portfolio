dd12858e356cf2158de86825b696b9466307937e
CrystalNyein <nyeinchanlay99@gmail.com>
2022-06-03 13:18:48 +0630

    Add Banner text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AppHeader from "./components/AppHeader";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="Banner">
+      <h1>
+        Hello! I'm <span>Nyein.</span>
+      </h1>
+      <p>
+        I have learnt website development in several languages. I have
+        experiences in writing Frontend, Backend as well as Database. I'm
+        flexible using different languages. Check out my projects below!
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
