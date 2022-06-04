a5163547bc172eaa2fa452afbcb106b66658cc26
CrystalNyein <nyeinchanlay99@gmail.com>
2022-06-04 15:28:03 +0630

    Handlescrolling testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import AboutMe from "./components/AboutMe";
 import AppHeader from "./components/AppHeader";
 import Banner from "./components/Banner";
 
 const App = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    setScrollPosition(window.pageYOffset);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition]);
   return (
     <div className="App">
       <AppHeader />
