8af6d5f716167ad631c9204539cfab93c48bcece
CrystalNyein <nyeinchanlay99@gmail.com>
2022-06-04 15:06:54 +0630

    Add about me title

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./AboutMe.css";
 
 const AboutMe = () => {
-  return <div className="AboutMe"></div>;
+  return (
+    <div className="AboutMe">
+      <h2>About Me</h2>
+    </div>
+  );
 };
 
 export default AboutMe;
