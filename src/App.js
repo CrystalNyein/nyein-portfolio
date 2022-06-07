8626ded175830774762430aeff4061c2e3b1bcbf
CrystalNyein <nyeinchanlay99@gmail.com>
2022-06-07 13:38:47 +0630

    remove warning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.css";
 import AboutMe from "./components/AboutMe";
 import AppHeader from "./components/AppHeader";
 import Banner from "./components/Banner";
 
+const sectionPosition = [0, 0, 0, 0];
+
 const App = () => {
-  const sectionPosition = [0, 0, 0, 0];
   const secRef1 = useRef(null);
   const secRef2 = useRef(null);
-  const secRef3 = useRef(null);
+  // const secRef3 = useRef(null);
   useEffect(() => {
     sectionPosition[1] = secRef1.current.offsetTop - 72;
     sectionPosition[2] = secRef2.current.offsetTop - 72;
