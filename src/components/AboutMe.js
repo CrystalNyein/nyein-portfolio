4b93acd41df3f64ce09e69d522842af41e9e8ce9
CrystalNyein <nyeinchanlay99@gmail.com>
2022-06-07 13:17:35 +0630

    Forward ref for position

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./AboutMe.css";
 
-const AboutMe = () => {
+const AboutMe = (props) => {
   return (
-    <div id="AboutMe" className="AboutMe">
+    <div id="AboutMe" className="AboutMe" ref={props.forwardedRef}>
       <h2>About Me</h2>
     </div>
   );
