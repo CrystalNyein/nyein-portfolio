69531694cb574816ddad14d1675f4c4c1e54ee54
CrystalNyein <nyeinchanlay99@gmail.com>
2024-12-31 11:29:11 +0630

    Tailwindcss config

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,12 @@
 const Navbar = () => {
-  return <header>Nav Bar</header>;
+  return (
+    <header>
+      <h1>Nyein Chan Lay</h1>
+      <h2>Full Stack Developer</h2>
+      <p>I build pixel-perfect, engaging and accessible digital experiences.</p>
+      <nav></nav>
+    </header>
+  );
 };
 
 export default Navbar;
