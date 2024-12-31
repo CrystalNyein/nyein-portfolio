0948754a30b33a31026ea4fa9508b171da12255b
CrystalNyein <nyeinchanlay99@gmail.com>
2024-12-31 10:29:05 +0630

    Fonts, app header html, dependencies

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,36 +1,36 @@
-import React, { useState, useRef, useEffect } from "react";
-import "./AppHeader.css";
+import React, { useState, useRef, useEffect } from 'react';
+import './AppHeader.css';
 
 const AppHeader = (props) => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [section, setSection] = useState("Home");
+  const [section, setSection] = useState('Home');
   const navLink = useRef(null);
   const toggleMenu = (e) => {
     if (menuOpen) {
-      e.target.classList.remove("fa-xmark");
-      e.target.classList.add("fa-burger");
-      navLink.current.classList.remove("active");
+      e.target.classList.remove('fa-xmark');
+      e.target.classList.add('fa-burger');
+      navLink.current.classList.remove('active');
     } else {
-      e.target.classList.remove("fa-burger");
-      e.target.classList.add("fa-xmark");
-      navLink.current.classList.add("active");
+      e.target.classList.remove('fa-burger');
+      e.target.classList.add('fa-xmark');
+      navLink.current.classList.add('active');
     }
     setMenuOpen(!menuOpen);
   };
   const handleScroll = (navSec) => {
     setSection(navSec);
     switch (navSec) {
-      case "Home":
-        window.scrollTo({ top: 0, behavior: "smooth" });
+      case 'Home':
+        window.scrollTo({ top: 0, behavior: 'smooth' });
         break;
-      case "About":
-        window.scrollTo({ top: props.sectionPosition[1], behavior: "smooth" });
+      case 'About':
+        window.scrollTo({ top: props.sectionPosition[1], behavior: 'smooth' });
         break;
-      case "Project":
-        window.scrollTo({ top: props.sectionPosition[2], behavior: "smooth" });
+      case 'Project':
+        window.scrollTo({ top: props.sectionPosition[2], behavior: 'smooth' });
         break;
-      case "Hire":
-        window.scrollTo({ top: props.sectionPosition[3], behavior: "smooth" });
+      case 'Hire':
+        window.scrollTo({ top: props.sectionPosition[3], behavior: 'smooth' });
         break;
       default:
     }
@@ -39,72 +39,98 @@ const AppHeader = (props) => {
     const setSectionVar = () => {
       const position = window.pageYOffset;
       if (position < props.sectionPosition[1]) {
-        setSection("Home");
+        setSection('Home');
       } else if (position < props.sectionPosition[2]) {
-        setSection("About");
+        setSection('About');
       } else if (position < props.sectionPosition[3]) {
-        setSection("Project");
+        setSection('Project');
       } else {
-        setSection("Hire");
+        setSection('Hire');
       }
     };
     setSectionVar();
-    window.addEventListener("scroll", setSectionVar, { passive: true });
+    window.addEventListener('scroll', setSectionVar, { passive: true });
     return () => {
-      window.removeEventListener("scroll", setSectionVar);
+      window.removeEventListener('scroll', setSectionVar);
     };
   }, [props.sectionPosition]);
   return (
-    <div className="AppHeader">
-      <div className="nav-bar">
-        <div className="nav-mobile">
-          <div className="nav-logo">CN</div>
-          <div className="nav-action" onClick={toggleMenu}>
-            <i className="fa-solid fa-burger"></i>
-          </div>
-        </div>
-        <ul className="nav-link" ref={navLink}>
-          <li className={section === "Home" ? "nav-item current" : "nav-item"}>
-            <button
-              onClick={() => {
-                handleScroll("Home");
-              }}
-            >
-              Home
-            </button>
+    // <div className="AppHeader">
+    //   <div className="nav-bar">
+    //     <div className="nav-mobile">
+    //       <div className="nav-logo">CN</div>
+    //       <div className="nav-action" onClick={toggleMenu}>
+    //         <i className="fa-solid fa-burger"></i>
+    //       </div>
+    //     </div>
+    //     <ul className="nav-link" ref={navLink}>
+    //       <li className={section === "Home" ? "nav-item current" : "nav-item"}>
+    //         <button
+    //           onClick={() => {
+    //             handleScroll("Home");
+    //           }}
+    //         >
+    //           Home
+    //         </button>
+    //       </li>
+    //       <li className={section === "About" ? "nav-item current" : "nav-item"}>
+    //         <button
+    //           onClick={() => {
+    //             handleScroll("About");
+    //           }}
+    //         >
+    //           About me
+    //         </button>
+    //       </li>
+    //       <li
+    //         className={section === "Project" ? "nav-item current" : "nav-item"}
+    //       >
+    //         <button
+    //           onClick={() => {
+    //             handleScroll("Projects");
+    //           }}
+    //         >
+    //           My Projects
+    //         </button>
+    //       </li>
+    //       <li className={section === "Hire" ? "nav-item current" : "nav-item"}>
+    //         <button
+    //           onClick={() => {
+    //             handleScroll("Hire");
+    //           }}
+    //         >
+    //           Hire me
+    //         </button>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
+    <header>
+      <h1>Nyein Chan Lay</h1>
+      <h2>Full Stack Web Developer</h2>
+      <p>I build pixel-perfect, engaging, and accesible digital experiences.</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="">About</a>
           </li>
-          <li className={section === "About" ? "nav-item current" : "nav-item"}>
-            <button
-              onClick={() => {
-                handleScroll("About");
-              }}
-            >
-              About me
-            </button>
+          <li>
+            <a href="">Experience</a>
           </li>
-          <li
-            className={section === "Project" ? "nav-item current" : "nav-item"}
-          >
-            <button
-              onClick={() => {
-                handleScroll("Projects");
-              }}
-            >
-              My Projects
-            </button>
-          </li>
-          <li className={section === "Hire" ? "nav-item current" : "nav-item"}>
-            <button
-              onClick={() => {
-                handleScroll("Hire");
-              }}
-            >
-              Hire me
-            </button>
+          <li>
+            <a href="">Projects</a>
           </li>
         </ul>
+      </nav>
+      <div>
+        <a>ICON</a>
+        <a>ICON</a>
+        <a>ICON</a>
+        <a>ICON</a>
+        <a>ICON</a>
+        <a>ICON</a>
       </div>
-    </div>
+    </header>
   );
 };
 
